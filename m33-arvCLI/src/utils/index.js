686aea819a949fc029a4322f374977cd8adc3d96
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-01-31 11:12:56 +0000

    basic init, Movie class and movArr exist

---
+++ m33-arvCLI/src/utils/index.js
@@ -0,0 +1,12 @@
+movieArr = [];
+class Movie {
+  constructor(title, actor = "Not specified") {
+    this.title = title;
+    this.actor = actor;
+  }
+
+  add() {
+    movieArr.push(this);
+  }
+}
+module.exports = { Movie, movieArr };
